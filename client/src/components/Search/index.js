18ad910560759f8cd3445ca7eab913a4c8a74b60
Khine Wai <khine.wai@uconn.edu>
2020-04-01 19:56:20 -0400

    Build Search

---
+++ client/src/components/Search/index.js
@@ -0,0 +1,14 @@
+// Index for the search folder
+import React, { Component } from "react";
+
+class Search extends Component {
+    state = {};
+
+    render() {
+        return (    
+            <input type="text" placeholder="search..."/>
+        );
+    }
+}
+
+export default Search;
