abd3373a83c64018b3f8d9eb489d600fd26982b0
Khine Wai <khine.wai@uconn.edu>
2020-04-01 20:23:44 -0400

    Search

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,10 +3,23 @@ import React, { Component } from "react";
 
 class Search extends Component {
     state = {};
+    
+    handleFormSubmit(event){
+        event.preventDefault()
+    console.log("spitout")
+    }
 
     render() {
-        return (    
+        return (   
+        <form>
             <input type="text" placeholder="search..."/>
+            <button
+          onClick={this.handleFormSubmit}
+          type="submit"
+        >
+          Search
+        </button>
+        </form> 
         );
     }
 }
