4c4907a2cb84de2ef78b912bfe155871e5185ccb
Khine Wai <khine.wai@uconn.edu>
2020-04-10 18:04:55 -0400

    push

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,9 +8,4 @@ router.use("/news", newsRoutes);
 // router.use("/user", userRoutes);
 
 
-// For anything else, render the html page
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
-
 module.exports = router;
